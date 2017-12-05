ad42fc0906529b876b5ef6d8eef2ee2b8e43d56f
Sharven <sharvenprasad101@gmail.com>
2017-12-04 21:45:38 -0500

    PAUSE MENU

--- We-will-name-this-later-too-09200e4efd7d37d5849b580f5636a8d75f6ace50/LD40/Assets/Custom Assets/Scripts/Other/MultiButton.cs
+++ We-will-name-this-later-too-09200e4efd7d37d5849b580f5636a8d75f6ace50/LD40/Assets/Custom Assets/Scripts/Other/MultiButton.cs
@@ -2,10 +2,27 @@
 
 public class MultiButton : MonoBehaviour {
 
-	
+	bool isPaused = false;
+	public GameObject panel;
 
-	void UnPause()
+	public void UnPause()
 	{
+		Time.timeScale = 1f;
+		isPaused = false;
+	}
 
+	private void Update()
+	{
+		panel.SetActive(isPaused);
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			isPaused = !isPaused;
+
+			if (isPaused)
+				Time.timeScale = 0f;
+
+			if (!isPaused)
+				Time.timeScale = 1f;
+		}
 	}
 }
